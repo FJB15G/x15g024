2d9c7f4e50fd6aadf5d2283da7c0f6968816bdee
unknown <x15g024@PC504N034.chiba-fjb.ac.jp>
2015-11-02 14:48:57 +0900

    課題4

---
+++ src/jp/ac/chibafjb/x15g024/kd101/s.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g024.kd101;
+
+public class s {
+
+	public static void main(String[] args) {
+		System.out.println("あいうえお あいうえお あいうえお");
+		System.out.println("あいうえお あいうえお あいうえお");
+		System.out.println("あいうえお あいうえお あいうえお");
+
+	}
+
+}

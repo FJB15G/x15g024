533e2837f3d67e0c6116ed90f8bde8ca997475a9
unknown <x15g024@PC504N034.chiba-fjb.ac.jp>
2015-11-02 14:56:58 +0900

    課題5

---
+++ src/jp/ac/chibafjb/x15g024/kd101/m.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g024.kd101;
+
+public class m {
+
+	public static void main(String[] args) {
+		System.out.println("氏名 : 船橋　太郎（ふなばし　たろう)");
+		System.out.println("○○： うぇあ");
+		System.out.println("××: うへぁ");
+
+	}
+
+}

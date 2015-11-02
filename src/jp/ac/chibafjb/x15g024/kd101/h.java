7430b44a13cd6ac9b924efac37aa976cb3a86c05
unknown <x15g024@PC504N034.chiba-fjb.ac.jp>
2015-11-02 14:40:33 +0900

    課題2

---
+++ src/jp/ac/chibafjb/x15g024/kd101/h.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g024.kd101;
+
+public class h {
+
+	public static void main(String[] args) {
+		System.out.println("1 船橋 太郎");
+		System.out.println("2 船橋 太郎");
+		System.out.println("3 船橋 太郎");
+		System.out.println("4 船橋 太郎");
+		System.out.println("5 船橋 太郎");
+		System.out.println("6 船橋 太郎");
+		System.out.println("7 船橋 太郎");
+		System.out.println("8 船橋 太郎");
+		System.out.println("9 船橋 太郎");
+		System.out.println("10 船橋 太郎");
+
+	}
+
+}

e7752300a2139af62c3c5dd1164f7cbe46e07eed
unknown <x15g024@PC504N034.chiba-fjb.ac.jp>
2015-11-02 14:46:04 +0900

    課題3

---
+++ src/jp/ac/chibafjb/x15g024/kd101/k.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g024.kd101;
+
+public class k {
+
+	public static void main(String[] args) {
+		System.out.println("1 船橋 太郎 2 船橋 太郎 3 船橋 太郎 4 船橋 太郎 5 船橋 太郎 6 船橋 太郎 7 船橋 太郎 8 船橋 太郎 9 船橋 太郎 10 船橋 太郎");
+		
+
+	}
+
+}

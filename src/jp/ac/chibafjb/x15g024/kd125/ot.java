de5476f1b84452f172243cf2d35ada7fd2587477
unknown <x15g024@PC504N034.chiba-fjb.ac.jp>
2015-11-16 13:43:37 +0900

    課題6

---
+++ src/jp/ac/chibafjb/x15g024/kd125/ot.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g024.kd125;
+
+public class ot {
+
+	public static void main(String[] args) {
+		int i=0;
+		int a=0;
+		for(i=0;i<3;i++)
+			System.out.println(a=a+10"*");
+
+	}
+
+}
